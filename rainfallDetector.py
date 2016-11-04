7aa4550212204ed0226c947b73cc681789d97848
Darryl Balderas <d_b118@txstate.edu>
2016-11-04 14:18:38 -0500

    create a class for Rainfall Detection System

--- rainfallDetector.py
+++ rainfallDetector.py
@@ -1,2 +1,31 @@
+import os 
+import sys
+import serial 
+import xbee 
+import time 
+
+class RainfallDetection:
+  
+  
+  def __init__(self, serialConnection, xbee_id ):
+    self.connection = serialConnection
+    self.id = xbee_id
+    
+  def stopConnection(self):
+    print "Function Not Done"
+    
+  def resetConnection():
+    print "Function Not Done"
+    
+  def showData():
+    print "Function Not Done"
+    
+  def establishConnection():
+    print "Function Not Done"
+
+    
+
+
+
 print "hello"
 
