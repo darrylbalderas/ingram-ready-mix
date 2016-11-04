93b4f225d69ef44ebd4aa345263c1bb936fa8bbf
Darryl Balderas <d_b118@txstate.edu>
2016-11-04 14:24:03 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+#this will be where we run our program and incorporate our SenseHat, XBee, Buzzer, LCD screen. 
+import os 
+import time 
+import sys 
+from RPi import GPIO
+
+print "default message: Hello World"
