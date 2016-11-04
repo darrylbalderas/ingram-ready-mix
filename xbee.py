e5b16b9344540bf1986f15a1a8041128a18b5e03
Darryl Balderas <d_b118@txstate.edu>
2016-11-04 14:27:34 -0500

    change the file name and added functions

---
+++ xbee.py
@@ -0,0 +1,38 @@
+import os 
+import sys
+import serial 
+import xbee 
+import time 
+
+class XBee:
+  
+  
+  def __init__(self, serialConnection, xbee_id, rainfall_xbee):
+    self.connection = serialConnection
+    self.id = xbee_id
+    self.rainfallXbee = rainfall_xbee
+    
+  def stopUsbConnection(self):
+    print "Function Not Done"
+    
+  def resetUsbConnection():
+    print "Function Not Done"
+    
+  def showUsbData():
+    print "Function Not Done"
+    
+  def establishXbeeConnection():
+    print "Function Not Done"
+    
+  def stopXbeeConnection():
+    print "Function Not Done"
+  
+  def resetUsbConnection():
+    print "Function Not Done"
+
+    
+
+
+
+print "hello"
+
