f6cf8f68734302ca85ccb20195e1853481da36e3
Darryl Balderas <d_b118@txstate.edu>
2016-11-04 14:00:18 -0500

    create class for Buzzer

--- buzzer.py
+++ buzzer.py
@@ -1,1 +1,19 @@
+import os 
+import sys
+import time 
+from RPi import GPIO
+
+class Buzzer:
+  
+  def __init__(self,gpioPin, delay, pause):
+    self.buzzer_delay = delay
+    self.pause_time = pause
+    self.gpioPin = gpioPin
+    
+  def stopBuzzer(self):
+    print "Function Not Done"
+    
+  def startBuzzer(self):
+    print "Function Not Done"
+    
 print "hello"
