58007c7aa06f7bd4133e0ac85237ab16a70bbf1b
Darryl Balderas <d_b118@txstate.edu>
2016-11-04 13:48:43 -0500

    creating a class for worker

--- worker.py
+++ worker.py
@@ -1,1 +1,26 @@
+import sys
+import os 
+import alarmSystem 
+import Buzzer
+
+class Worker:
+  
+  name = "Ingram Worker"
+  totalTimeSample = 15
+  
+  def __init__(self, alarmSystem):
+    self.alarmsystem = alarmSystem
+  
+  def startAlarmSystem(self, gpioPins):
+    print "Function Not Done"
+    
+  def stopAlarmSystem(self, gpioPins):
+    print "Function Not Done
+    
+  def ResetSystem(self, gpioPins):
+    print "Function Not Done"
+    
+    
+    
+    
 print "hello"
