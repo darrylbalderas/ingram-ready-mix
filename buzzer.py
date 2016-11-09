1fbaf0b919dd396b05fb130effdea7adfb706151
Darryl Balderas <d_b118@txstate.edu>
2016-11-09 11:40:26 -0600

    Update buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -1,14 +1,17 @@
 import os 
 import sys
 import time 
+import math 
 from RPi import GPIO
+
 
 class Buzzer:
   
-  def __init__(self,gpioPin, delay, pause):
-    self.buzzer_delay = delay
-    self.pause_time = pause
+  def __init__(self,gpioPin, delay, pause,frequency):
+    self.buzzerDelay = delay
+    self.pauseTime = pause
     self.gpioPin = gpioPin
+    self.frequency = frequency 
     
   def stopBuzzer(self):
     print "Function Not Done"
@@ -16,4 +19,22 @@ class Buzzer:
   def startBuzzer(self):
     print "Function Not Done"
     
-print "hello"
+  def getFrequency(self):
+    return self.frequency
+  
+  def setFrequency(self,newFrequency):
+    self.frequency = newFrequency 
+    
+  def getBuzzerDelay(self):
+    return self.buzzerDelay
+  
+  def setBuzzerDelay(self, newBuzzerDelay):
+    self.buzzerDelay = newBuzzerDelay
+  
+  def getPauseTime(self):
+    return self.pauseTime
+  
+  def setPauseTime(self,newPauseTime):
+    self.pauseTime = newPauseTime
+    
+    
