2748d074debd25f336f54ab19c3f61bda9a8e86b
Darryl Balderas <d_b118@txstate.edu>
2016-11-21 19:19:26 -0600

    Update xbee.py

--- xbee.py
+++ xbee.py
@@ -1,14 +1,17 @@
-import os 
+import serial
+import glob
 import sys
-import serial 
-import xbee 
-import time 
+import requests
+
+system_name = sys.platform
+baud_Rate = 9600
 
 class XBee:
-  def __init__(self, serialConnection, xbee_id, rainfall_xbee):
+  def __init__(self, baudRate, xbee_id, rainfall_xbee):
     self.connection = serialConnection
     self.id = xbee_id
     self.rainfallXbee = rainfall_xbee
+    self.port_Number = self.xbee_Usb_Port()
     
   def stopUsbConnection(self):
     print "Function Not Done"
@@ -28,3 +31,20 @@ class XBee:
   def resetUsbConnection():
     print "Function Not Done"
     
+
+  def xbee_Usb_Port():
+      ports = glob.glob('/dev/tty[A-Za-z]*')
+      result = []
+      for port in ports:
+          try:
+              s = serial.Serial(port)
+              s.close()
+              result.append(port)
+          except( OSError, serial.SerialException):
+              pass
+      return result[0]
+
+xbee_Bravo = serial.Serial(port_Name, baud_Rate, timeout=0.5)
+
+
+
