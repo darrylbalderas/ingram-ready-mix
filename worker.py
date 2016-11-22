0d1ac6e5a856c13dab5599dab2f65560e0942be8
Darryl Balderas <d_b118@txstate.edu>
2016-11-21 19:31:24 -0600

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,23 +1,42 @@
 import sys
 import os 
 import alarmSystem 
+import time 
 import Buzzer
 import math 
 
 class Worker:
   
-  name = "Ingram Worker"
-  totalTimeSample = 15
-  
-  def __init__(self, alarmSystem):
+  name = "Ingram Employee"
+  def __init__(self, alarmSystem,buzzer):
     self.alarmsystem = alarmSystem
-    
-  def startAlarmSystem(self, gpioPins):
+    self.start_time = 0
+    self.end_time = 0
+    self.buzzer = buzzer
+   
+  def start_AlarmSystem(self, gpioPins):
     print "Function Not Done"
     
-  def stopAlarmSystem(self, gpioPins):
+  def stop_AlarmSystem(self, gpioPins):
     print "Function Not Done
     
-  def ResetSystem(self, gpioPins):
+  def sample_collected(self, gpioPins):
     print "Function Not Done"
+    
+  def sample_missed(self,gpioPins):
+    print "Function Not Done"
+    
+  def restart_System(self,gpioPins):
+    print "Function Not Done"
+    
+  def get_startTime(self):
+    return self.start_time
   
+  def set_startTime(self,new_time):
+    self.start_time = new_time
+    
+  def get_endTime(self):
+    return self.start_time
+  
+  def set_endTime(self,new_time):
+    self.end_time = new_time
