e3a9766e6514a48e8322658362be9bc83fe83f92
db118 <d_b118@txstate.edu>
2016-12-02 22:08:29 -0600

    editing xbee file

--- transmitter.py
+++ transmitter.py
@@ -4,32 +4,30 @@ import sys
 import requests
 from xbee import XBee
 
-system_name = sys.platform
-
+#Sychronous mode
 class Transmitter:
-  def __init__(self, baudRate, xbee_id):
+  def __init__(self, baudRate, dest_id):
     self.port_Number = self.xbee_Usb_Port()
     self.baudRate = baudRate
-    self.connection = serial.Serial(self.port_Number, baud_Rate, timeout=0.5)
-    self.id = xbee_id
+    self.connection = serial.Serial(self.port_Number, baudRate)
     self.xb = XBee(connection)
+    self.dest_id = dest_id
 
   def stopUsbConnection(self):
     self.connection.close()
     
   def resetUsbConnection(self):
     self.connection.close()
-    self.connection = serial(self.port_Number,self.baudRate, timeout =0.5)
+    self.connection = serial(self.port_Number,self.baudRate)
+
+  def sendXbeeData(self,command):
+    self.xb.send('at',frame_id = self.dest_id, command = command)
 
   def readXbeeData(self):
     self.xb.wait_read_frame()
-    
-  def establishXbeeConnection():
-    print "Function Not Done"
-    
-    
 
-  def xbee_Usb_Port():
+    
+  def xbee_Usb_Port(self):
       ports = glob.glob('/dev/tty[A-Za-z]*')
       result = []
       for port in ports:
@@ -39,9 +37,13 @@ class Transmitter:
               result.append(port)
           except( OSError, serial.SerialException):
               pass
-      return result[0]
+      print result
+      
 
 
+def message_received(data):
+  print data
 
+xb = Transmitter(9600,'A')
 
 
