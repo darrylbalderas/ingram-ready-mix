e4aebaf7a85599ef6b60f4e85395cc0fe084b3e0
db118 <d_b118@txstate.edu>
2017-01-10 21:48:20 -0600

    feat: update transmitter.py and receiver.py
    Created receiving and sending features for transmitter and
    receiver. features need testing

--- receiver.py
+++ receiver.py
@@ -3,23 +3,41 @@ import glob
 import sys
 from time import sleep 
 from xbee import ZigBee
+import pprint 
+
 # from time import sleep
 
-class Transmitter:
-  def __init__(self, baud_rate,port_num):
+class Receiver:
+  def __init__(self, baud_rate,port_num,SHSL):
+    self.SHSL = SHSL
     self.port_number = port_num
     self.baud_rate = baud_rate
-    self.connection = serial.Serial(self.port_number, baud_rate)
+    self.connection = serial.Serial(self.port_number, baud_rate, timeout = 1)
     self.xb = ZigBee(self.connection)
 
-
-  def stopUsbConnection(self):
+  def stopZigBee(self):
+    self.xb.halt()
     self.connection.close()
     
-  def resetUsbConnection(self):
+  def resetZigBee(self):
     self.connection.close()
     self.connection = serial(self.port_number,self.baud_rate)
+ 
+  def send_data(self,frame_id,dest_addr_long,dest_addr,data):
+    self.xb.send('tx',frame_id=frame_id,
+                 dest_addr_long = dest_addr_long,
+                 dest_addr=dest_addr,
+                 data = data)
 
+  def receive_data(self):
+    message = self.xb.wait_read_frame()
+    print(message)
+
+  def get_SHSL(self):
+    return self.SHSL
+
+  def set_SHSL(self,SHSL):
+    self.SHSL = SHSL
 
 def xbee_Usb_Port():
   if sys.platform.startswith('darwin'):
@@ -37,10 +55,22 @@ def xbee_Usb_Port():
   return result
 
 def main():
-  usb_list = xbee_Usb_Port()
-  # Create API object
-  xbee = Transmitter(9600,usb_list[1])
 
+  charlie_SHSL = b'\x00\x13\xA2\x00\x41\x04\x96\x6E'
+
+  broadcaster = b'\xFF\xFE'
+
+  bravo_SHSL = b'\x00\x13\xA2\x00\x41\x03\xF0\xFF'
+  coordinator = b'\x00\x00'
+
+  usb_list = xbee_Usb_Port()
+  charlie_xbee = Receiver(9600,usb_list.pop(),charlie_SHSL)
+  charlie_xbee.send_data(frame_id = b'\x01', 
+                         dest_addr_long = bravo_SHSL,
+                         dest_addr = coordinator, data = b'\x11')
+
+  charlie_xbee.receive_data()
+  charlie_xbee.stopZigBee()
 
 if __name__ == '__main__':
   main()
--- transmitter.py
+++ transmitter.py
@@ -6,20 +6,36 @@ from xbee import ZigBee
 # from time import sleep
 
 class Transmitter:
-  def __init__(self, baud_rate,port_num):
+  def __init__(self, baud_rate,port_num,SHSL):
+    self.SHSL = SHSL
     self.port_number = port_num
     self.baud_rate = baud_rate
-    self.connection = serial.Serial(self.port_number, baud_rate)
+    self.connection = serial.Serial(self.port_number, self.baud_rate)
     self.xb = ZigBee(self.connection)
 
-
-  def stopUsbConnection(self):
+  def stopZigBee(self):
+    self.xb.halt()
     self.connection.close()
     
-  def resetUsbConnection(self):
+  def resetZigBee(self):
     self.connection.close()
     self.connection = serial(self.port_number,self.baud_rate)
 
+  def send_data(self,frame_id,dest_addr_long,dest_addr,data):
+    self.xb.send('tx',frame_id=frame_id,
+                 dest_addr_long = dest_addr_long,
+                 dest_addr=dest_addr,
+                 data = data)
+
+  def receive_data(self):
+    message = self.xb.wait_read_frame()
+    print(message)
+
+  def get_SHSL(self):
+    return self.SHSL
+
+  def set_SHSL(self,SHSL):
+    self.SHSL = SHSL
 
 def xbee_Usb_Port():
   if sys.platform.startswith('darwin'):
@@ -36,10 +52,22 @@ def xbee_Usb_Port():
           pass
   return result
 
+
 def main():
+  bravo_SHSL = b'\x00\x13\xA2\x00\x41\x03\xF0\xFF'
+
+  charlie_SHSL = b'\x00\x13\xA2\x00\x41\x04\x96\x6E'
+
+  broadcaster = b'\xFF\xFE'
+  coordinator = b'\x00\x00'
+
   usb_list = xbee_Usb_Port()
-  # Create API object
-  xbee = Transmitter(9600,usb_list[0])
+  xbee = Transmitter(9600,usb_list[0],bravo_SHSL)
+  xbee.send_data(frame_id = b'\x01', 
+            dest_addr_long = charlie_SHSL,
+            dest_addr = broadcaster, data= b'\x11')
+  xbee.receive_data()
+  xbee.stopZigBee()
 
 
 if __name__ == '__main__':
