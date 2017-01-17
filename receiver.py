112e4c870dce854743279e79b75eb441439a93c9
db118 <d_b118@txstate.edu>
2017-01-17 16:23:13 -0600

    docs: add docstrings to class and functions

--- receiver.py
+++ receiver.py
@@ -1,10 +1,13 @@
 import serial
 import glob
 import sys
-from time import sleep 
+# from time import sleep 
 #from xbee import ZigBee
 from xbee import XBee 
 
+'''
+This Receiver class will resemble our bravo xbee module.
+'''
 class Receiver:
   def __init__(self, baud_rate,port_path,SHSL):
     self.SHSL = SHSL
@@ -14,29 +17,52 @@ class Receiver:
     self.xbee = XBee(self.ser, escaped=True)
 
   def stopXBee(self):
+    '''
+    Stops Xbee connection 
+    '''
     self.xbee.halt()
     self.ser.close()
     
   def resetXBee(self):
+    '''
+    Reset Serial connection and creates a new Serial Connection
+    '''    
     self.xbee.halt()
     self.ser.close()
     self.ser = serial(self.port_path,self.baud_rate)
  
   def send_data(self,dest_addr_long, data):
+    '''
+    Sends data to another Xbee with a certain destition address specified
+    '''
     self.xbee.tx_long_addr( dest_addr_long = dest_addr_long, data = data)
 
   def receive_data(self):
+    '''
+    Waits until a message is received 
+    '''
     message = self.xbee.wait_read_frame()
     return message
 
   def get_SHSL(self):
+    '''
+    Returns 64bit Xbee address
+    '''
     return self.SHSL
 
   def set_SHSL(self,SHSL):
+    '''
+    Changes the 64bit Xbee address
+    '''
     self.SHSL = SHSL
 
 
 def xbee_Usb_Port():
+  '''
+  Search in your file directory to find Usb port 
+  that your Xbee is connected to. Supports MacOs and 
+  linux operating system. Returns a list of usb ports. 
+  '''
   result = []
   if sys.platform.startswith('darwin'):
     ports = glob.glob('/dev/tty.usb*')
@@ -52,25 +78,32 @@ def xbee_Usb_Port():
           pass
   return result
 
-def toHex(s):
-    lst = []
-    for ch in s:
-        hv = hex(ord(ch)).replace('0x', '')
-        if len(hv) == 1:
-            hv = '0'+hv
-        hv = '0x' + hv
-        lst.append(hv)
+def toHex(sample):
+  '''
+  Changes a bit string and converts it to Hexadecimal 
+  '''
+  lst = []
+  for ch in sample:
+      hv = hex(ord(ch)).replace('0x', '')
+      if len(hv) == 1:
+          hv = '0'+hv
+      hv = '0x' + hv
+      lst.append(hv)
 
 def decodeReceivedFrame(data):
-            source_addr_long = toHex(data['source_addr_long'])
-            source_addr = toHex(data['source_addr'])
-            xBee_id = toHex(data['id'])
-            samples = toHex(data['samples'])
-            return [source_addr_long, source_addr, xBee_id, samples]
+  '''
+  Data received from the Xbee is received in a dictionary.
+  This function decodes using keys and returns a list with values 
+  associated with those keys
+  '''
+  source_addr_long = toHex(data['source_addr_long'])
+  source_addr = toHex(data['source_addr'])
+  xBee_id = toHex(data['id'])
+  samples = toHex(data['samples'])
+  return [source_addr_long, source_addr, xBee_id, samples]
 
 def main():
   charlie_SHSL = b'\x00\x13\xA2\x00\x41\x04\x96\x6E'
-
   #default_coordinator = b'\x00\x00\x00\x00\x00\x00\x00\x00'
 
   usb_list = xbee_Usb_Port()
@@ -90,7 +123,7 @@ def main():
         flag = True
       else:
         print("Data has not been received")
-    
+
     except KeyboardInterrupt:
       break
 
--- transmitter.py
+++ transmitter.py
@@ -7,55 +7,93 @@ from xbee import XBee
 # from xbee import ZigBee
 # from time import sleep
 
+'''
+This Transmitter class was created for the sole purpose 
+of testing our Xbee modules. In our senior design project,
+the role of transmitter will be assigned to Team Charlie
+since we have to wait for their signal to invoke our functionality
+'''
+
 class Transmitter:
   def __init__(self, baud_rate, port_path, SHSL):
-    self.SHSL = SHSL
-    self.port_path = port_path
+    self.SHSL = SHSL  #it is the specific xbee address in which you can send data
+    self.port_path = port_path 
     self.baud_rate = baud_rate
     self.ser = serial.Serial(self.port_path, self.baud_rate)
     self.xbee = XBee(self.ser, escaped=True)
 
   def stopXBee(self):
+    '''
+    Stops Xbee connection 
+    '''
     self.xbee.halt()
     self.ser.close()
     
   def resetXBee(self):
+    '''
+    Reset Serial connection and creates a new Serial Connection
+    '''
     self.ser.halt()
     self.ser.close()
     self.ser = serial(self.port_number,self.baud_rate)
 
   def send_data(self,dest_addr_long,data):
+    '''
+    Sends data to another Xbee with a certain destition address specified
+    '''
     self.xbee.tx_long_addr(dest_addr_long=dest_addr_long, data=data)
 
   def receive_data(self):
+    '''
+    Waits until a message is received 
+    '''
     message = self.xbee.wait_read_frame()
     return message
 
   def get_SHSL(self):
+    '''
+    Returns 64bit Xbee address
+    '''
     return self.SHSL
 
   def set_SHSL(self,SHSL):
+    '''
+    Changes the 64bit Xbee address
+    '''
     self.SHSL = SHSL
 
 
 def toHex(sample):
-    lst = []
-    for character in sample:
-        hv = hex(ord(character)).replace('0x', '')
-        if len(hv) == 1:
-            hv = '0'+hv
-        hv = '0x' + hv
-        lst.append(hv)
+  '''
+  Changes a bit string and converts it to Hexadecimal 
+  '''
+  lst = []
+  for character in sample:
+      hv = hex(ord(character)).replace('0x', '')
+      if len(hv) == 1:
+          hv = '0'+hv
+      hv = '0x' + hv
+      lst.append(hv)
 
 def decodeReceivedFrame(data):
-            source_addr_long = toHex(data['source_addr_long'])
-            source_addr = toHex(data['source_addr'])
-            xBee_id = toHex(data['id'])
-            samples = toHex(data['samples'])
-            return [source_addr_long, source_addr, xBee_id, samples]
+  '''
+  Data received from the Xbee is received in a dictionary.
+  This function decodes using keys and returns a list with values 
+  associated with those keys
+  '''
+  source_addr_long = toHex(data['source_addr_long'])
+  source_addr = toHex(data['source_addr'])
+  xBee_id = toHex(data['id'])
+  samples = toHex(data['samples'])
+  return [source_addr_long, source_addr, xBee_id, samples]
 
 
 def xbee_Usb_Port():
+  '''
+  Search in your file directory to find Usb port 
+  that your Xbee is connected to. Supports MacOs and 
+  linux operating system. Returns a list of usb ports. 
+  '''
   result = []
   if sys.platform.startswith('darwin'):
     ports = glob.glob('/dev/tty.usb*')
@@ -87,10 +125,10 @@ def main():
   while not flag:
     try:
       bravo_xbee.send_data(dest_addr_long = default_broadcaster, data=sending_data)
-      #sleep(0.1)
       data = bravo_xbee.receive_data()    
 
       if data['deliver_status'] != b'"':
+
         print("Data has been received ")
         flag = True
       else:
