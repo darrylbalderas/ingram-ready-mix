09eecbe4586103565504d3ad95afe384d5997011
db118 <d_b118@txstate.edu>
2017-02-25 11:43:49 -0600

    constructing the working flow of our project

--- bravo_main.py
+++ bravo_main.py
@@ -17,8 +17,9 @@ if __name__ == "__main__":
   bravo_xbee = Transceiver(9600,xbee_port)#,b"\x00\x13\xA2\x00\x41\x04\x96\x6E")
   led_matrix = LedMatrix()
   led_matrix.change_color(led_matrix.get_greenImage())
-  led_matrix.clear_matrix()
-  outfall_detection(bravo_xbee,lcd,led_matrix)
+  if '
+  print(os.environ['invoke'])
+  #outfall_detection(bravo_xbee,lcd,led_matrix)
   ##create rainfall_detection thread
   ## pass the rainfall detection function 
   ##create outfall detection thread
