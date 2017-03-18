869ff054603b6b7a526c8d91130d28267bc2b219
Michael_Rodriguez <mmr94@txstate.edu>
2017-03-17 19:34:32 -0500

    Update level_sensor.py
    Working level_sensor functon

--- charlie_system/level_sensor.py
+++ charlie_system/level_sensor.py
@@ -1,66 +1,74 @@
 import RPi.GPIO as GPIO
 
+import time
+
+#Import numpy to send the median on the recorded pool level values
+import numpy as np
+
+# Import the ADS1x15 module.
+import Adafruit_ADS1x15
+
+
+
 class LevelSensor:
-	def __init__(self,pin):
-		self.pin = pin
-		self.floor_rating = -0.10
-		self.ceiling_rating = 0.10
-		self.pool_level = 12
-		self.floor_level = 2190
-		self.ceiling_level = 2200
+    def __init__(self):
+        self.pool_level = 8
+        self.adc = Adafruit_ADS1x15.ADS1115()
+        self.GAIN = 1
+        
 
-	def check_value(self):
-		return GPIO.input(self.pin)
+    def resistance_to_inches(self, pool_level):
+        inches_left = 8
+        
+        if pool_level >= 15000 and pool_level <= 157000: #Between 0 to 1 inches filled on tap
+            inches_left = 7
+        elif pool_level >= 14300 and pool_level <= 14999: #Between 1 to 2 inches filled on tape
+            inches_left = 6
+        elif pool_level >= 13300 and pool_level <= 14299: #Between 2 to 3 inches filled on tape
+            inches_left = 5
+        elif pool_level >= 12200 and pool_level <= 13299: #Between 3 to 4 inches filled on tape
+            inches_left = 4
+        elif pool_level >= 11100 and pool_level <= 12199: #Between 4 to 5 inches filled on tape
+            inches_left = 3
+        elif pool_level >= 9000 and pool_level <= 11099: #Between 5 to 6 inches filled on tape
+            inches_left = 2
+        elif pool_level >= 7600 and pool_level <= 8999: #Between 6 to 7 inches filled on tape
+            inches_left = 1
+        elif pool_level >= 4000 and pool_level <= 7599: #Between 7 to 8 inches filled on tape
+            inches_left = 0
 
-	def resistance_to_inches(self):
-		inches_left = 12
+        return inches_left
 
-		if self.floor_level >= 2190 and self.ceiling_level <= 2200: #Between 0 to 1 inches filled on tap
-			inches_left = 12
-		elif self.floor_level  >= 2020 and self.ceiling_level <= 2189: #Between 1 to 2 inches filled on tape
-			inches_left = 11
-		elif self.floor_level  >= 1890 and self.ceiling_level <= 2019: #Between 2 to 3 inches filled on tape
-                        inches_left = 10
-		elif self.floor_level  >= 1710 and self.ceiling_level <= 1889: #Between 3 to 4 inches filled on tape
-                        inches_left = 9
-		elif self.floor_level  >= 1550 and self.ceiling_level <= 1709: #Between 4 to 5 inches filled on tape
-                        inches_left = 8
-		elif self.floor_level  >= 1390 and self.ceiling_level <= 1549: #Between 5 to 6 inches filled on tape
-			inches_left = 7
-		elif self.floor_level  >= 1220 and self.ceiling_level <= 1389: #Between 6 to 7 inches filled on tape
-			inches_left = 6
-		elif self.floor_level  >= 1080 and self.ceiling_level <= 1219: #Between 7 to 8 inches filled on tape
-                        inches_left = 5
-		elif self.floor_level  >= 910 and self.ceiling_level <= 1079: #Between 8 to 9 inches filled on tape
-                        inches_left = 4
-		elif self.floor_level  >= 770 and self.ceiling_level <= 909: #Between 9 to 10 inches filled on tape
-                        inches_left = 3
-		elif self.floor_level  >= 610 and self.ceiling_level <= 769: #Between 10 to 11 inches filled on tape
-                        inches_left = 2
-		elif self.floor_level  >= 420 and self.ceiling_level <= 609: #Between 11 to 12 inches filled on taped
-                        inches_left = 1
-		elif self.floor_level  >= 380 and self.ceiling_level <= 419: #Between 12 inches filled on taped
-                        inches_left = 0
+    def get_pool_level(self):
+        #eTape Continuous Fluid Level PN 12110215TC-8
+        num_list = []
+        pool_level = 0
 
-		return inches_left
+        for x in xrange(30):
+                value = self.adc.read_adc_difference(0, gain=self.GAIN)
+                num_list.append(value)
+                time.sleep(0.5)
 
-	def get_pool_level(self):
-	    #eTape Continuous Fluid Level PN-12110215TC-12
-	    #Sensor Output: 2250ohms empty, 400 ohms full +- 10%
-	    data = self.check_value()
-	    self.floor_level = data + (data * self.floor_rating)
-	    self.ceiling_level = data + (data * self.ceiling_rating)
-	    self.pool_level = self.resistance_to_inches()
-	      
-	    #Create the rules here to see the amount of inches needed for outfall
-	    #pool_level equals the amount left for outfall to occur
-	    return self.pool_level
+                
+        pool_level = np.median(num_list)
 
-	def check_overflow(self):
-		if self.pool_level == 0:
-			return True
-		else:
-			return False
+        self.pool_level = self.resistance_to_inches(pool_level)
+          
+        #Create the rules here to see the amount of inches needed for outfall
+        #pool_level equals the amount left for outfall to occur
+        return self.pool_level
+
+    def check_overflow(self):
+        if self.pool_level == 0:
+            return True
+        else:
+            return False
+
+def main():
+        level = LevelSensor()
+        print(level.get_pool_level())
+
+main()
 
 
 
