8c212ca746dd381e03d0d5101fe3af07b5917728
db118 <d_b118@txstate.edu>
2017-03-20 22:33:49 -0500

    testing the buzzers

--- test_buzzers.py
+++ test_buzzers.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as gpio
 from time import sleep
-
+gpio.setmode(gpio.BCM)
 buzzers = [4,17,22,27,5,6,13,19] ## wiring in beardboard
 
 def initalize_buzzers(buzzers):
@@ -17,11 +17,18 @@ def stop_buzzer():
 
 
 def main():
-	initalize_buzzers(buzzers)
-	while True:
-		input_value = raw_input("Enter a key: (Turn on buzzers (Y or y)")
-		if input_value.lower() == "y":
-			print("Buzzing for three seconds")
-			start_buzzer()
-			sleep(3)
-			stop_buzzer()
+    initalize_buzzers(buzzers)
+    while True:
+          try:
+            input_value = raw_input("Enter a key: (Turn on buzzers (Y or y):  ")
+            if input_value.lower() == "y":
+              print("Buzzing for 2 seconds")
+              start_buzzer()
+              sleep(2)
+              stop_buzzer()
+          except KeyboardInterrupt:
+            print("\nExiting program")
+
+
+if __name__ == "__main__":
+  main()
