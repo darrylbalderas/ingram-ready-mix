d065b561ad48cfd47e2ac2382eb79c5800fd0c61
db118 <d_b118@txstate.edu>
2017-03-21 22:16:47 -0500

    adding debugging statment for the multithreading scripts

--- test_files/multi_thread_receive.py
+++ test_files/multi_thread_receive.py
@@ -7,44 +7,44 @@ from time import time
 from threading import Thread
 from threading import Lock
 from threading import Event
-import RPi.GPIO as GPIO
+##import RPi.GPIO as gpio
 from transceiver import Transceiver
 
-complete = 12 
-mute = 20
-miss = 16
-restart = 21
-gpio.setmode(gpio.BCM)
-gpio.setup(complete,gpio.IN)
-gpio.setup(mute,gpio.IN)
-gpio.setup(miss,gpio.IN)
-gpio.setup(restart,gpio.IN)
-
-def check_complete():
-	return gpio.input(complete)
-
-def check_miss():
-	return gpio.input(miss)
-
-def check_mute():
-	return gpio.input(mute)
-
-def check_restart():
-	return gpio.input(restart)
-
-def check_buttons():
-	print("This is the value for complete button: %d" %(check_complete()))
-    print("This is the value for missed button: %d" %(check_miss()))
-    print("This is the value for mute button: %d" %(check_mute()))
-    print("This is the value for restart button: %d" %(check_restart()))
-    print('\n\n')
+##complete = 12 
+##mute = 20
+##miss = 16
+##restart = 21
+##gpio.setmode(gpio.BCM)
+##gpio.setup(complete,gpio.IN)
+##gpio.setup(mute,gpio.IN)
+##gpio.setup(miss,gpio.IN)
+##gpio.setup(restart,gpio.IN)
+##
+##def check_complete():
+##    return gpio.input(complete)
+##
+##def check_miss():
+##    return gpio.input(miss)
+##
+##def check_mute():
+##    return gpio.input(mute)
+##
+##def check_restart():
+##    return gpio.input(restart)
+##
+##def check_buttons():
+##    print("This is the value for complete button: %d" %(check_complete()))
+##    print("This is the value for missed button: %d" %(check_miss()))
+##    print("This is the value for mute button: %d" %(check_mute()))
+##    print("This is the value for restart button: %d" %(check_restart()))
+##    print('\n\n')
 
 def send_confirmation(xbee,lock):
   message = ""
   while not message == 'tri':
-  	if check_mute() and check_complete() and check_restart() and check_miss():
-  		check_buttons()
-  	message = xbee.receive_message()
+##    if check_mute() and check_complete() and check_restart() and check_miss():
+##        check_buttons()
+    message = xbee.receive_message()
   lock.acquire()
   xbee.send_message("tyes\n")
   sleep(0.5)
@@ -61,11 +61,9 @@ def receive_data(bravo_xbee):
     if len(message) != 0:
       if message[0] == 'r' and not rain_flag:
         rain_val = bravo_xbee.remove_character(message,'r')
-        set_value_file(RAIN,rain_val)
         rain_flag = True
       elif message[0] == 'p' and not pool_flag:
         pool_val = bravo_xbee.remove_character(message,'p')
-        set_value_file(POOL_LEVEL,pool_val)
         pool_flag = True
     bravo_xbee.send_message("rno\n")
     sleep(0.5)
@@ -76,10 +74,13 @@ def receive_data(bravo_xbee):
 def detect_rain(bravo_xbee,lock,event):
   while not event.isSet():
     send_confirmation(bravo_xbee,lock)
+    print("got first rain confirmation")
     start_timeDate = datetime.datetime.now()
     send_confirmation(bravo_xbee,lock)
+    print("got second rain confirmation")
     lock.acquire()
     rain_fall, pool_level = receive_data(bravo_xbee)
+    print("got pool and rain data")
     lock.release()
     end_timeDate = datetime.datetime.now() 
     end_time = '%s:%s:%s'%(end_timeDate.hour,end_timeDate.minute,end_timeDate.second)
@@ -97,24 +98,25 @@ def send_outfall_conf(xbee,lock):
 
 def detect_outfall(bravo_xbee,lcd,led_matrix,lock,event):
   while not event.isSet():
-  	send_outfall_conf(bravo_xbee,lock)
-  	print("got it")
+    send_outfall_conf(bravo_xbee,lock)
+    print("got the outfall confirmation")
 
 def main():
-	try:
-		lock = Lock()
-		event = Event()
-		xbee_port = xbee_usb_port()
-		if xbee_port != None:
-			bravo_xbee = Transceiver(9600,xbee_port)
-			thread1 = Thread(target=outfall_detection, args=(bravo_xbee,lock,event,))
-			thread2 = Thread(target=rain_detection, args=(bravo_xbee,lock,event,))
-			thread1.start()
-			thread2.start()
-		else:
-			print("Check the Xbee and LCD connection")
-		except KeyboardInterrupt:
-			event.set()
-			print("Ending the Program")
-			thread1.join()
-			thread2.join()
+    try:
+        lock = Lock()
+        event = Event()
+        xbee_port = xbee_usb_port()
+        if xbee_port != None:
+            bravo_xbee = Transceiver(9600,xbee_port)
+            print("starting the threads")
+            thread1 = Thread(target=outfall_detection, args=(bravo_xbee,lock,event,))
+            thread2 = Thread(target=rain_detection, args=(bravo_xbee,lock,event,))
+            thread1.start()
+            thread2.start()
+        else:
+            print("Check the Xbee connection")
+    except KeyboardInterrupt:
+            event.set()
+            print("Ending the Program")
+            thread1.join()
+            thread2.join()
--- test_files/multi_thread_send.py
+++ test_files/multi_thread_send.py
@@ -7,7 +7,7 @@ from time import time
 from threading import Thread
 from threading import Lock
 from threading import Event
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
 from transceiver import Transceiver
 
 flow = 23
@@ -20,114 +20,120 @@ gpio.setup(level,gpio.IN)
 gpio.setup(rain,gpio.IN)
 
 def xbee_usb_port():
-	if sys.platform.startswith('linux'):
-	    ports = glob.glob('/dev/ttyU*')
-	if len(ports) != 0:
-		result = []
-		for port in ports:
-			try:
-				ser = serial.Serial(port)
-				ser.close()
-				result.append(port)
-			except( OSError, serial.SerialException):
-				pass
-		return result[0]
-	else:
-		return None
+    if sys.platform.startswith('linux'):
+        ports = glob.glob('/dev/ttyU*')
+    if len(ports) != 0:
+        result = []
+        for port in ports:
+            try:
+                ser = serial.Serial(port)
+                ser.close()
+                result.append(port)
+            except( OSError, serial.SerialException):
+                pass
+        return result[0]
+    else:
+        return None
 
 def check_rainguage():
-	return gpio.input(rain)
+    return gpio.input(rain)
 
 def check_flowsensor():
-	return gpio.input(flow)
+    return gpio.input(flow)
 
 def check_levelsensor():
-	return gpio.input(level)
+    return gpio.input(level)
 
 def get_tick():
-	previous_state = 0
-	previous_time = time()
-	collection_duration = 3
-	while (time()-previous_time) <= collection_duration:
-		current_state = check_rainguage()
-		if current_state  > previous_state:
-			previous_state = current_state
-			while current_state == previous_state:
-				current_state = check_rainguage()
-			return 1
-	return 0
+    previous_state = 0
+    previous_time = time()
+    collection_duration = 2
+    while (time()-previous_time) <= collection_duration:
+        current_state = check_rainguage()
+        if current_state  > previous_state:
+            previous_state = current_state
+            while current_state == previous_state:
+                current_state = check_rainguage()
+            return 1
+    return 0
 
 def get_total_rainfall():
-	counter = 0
+    counter = 0
     rainfall = 2.769
-	ticking = 0
-	previous_time = time()
-	while (time()-previous_time) <= max_time:
-		ticking = get_tick()
-		if not ticking:
-			break
-		else:
-			rainfall += 2.769
-	return rainfall
+    ticking = 0
+    previous_time = time()
+    while (time()-previous_time) <= max_time:
+        ticking = get_tick()
+        if not ticking:
+            break
+        else:
+            rainfall += 2.769
+    return rainfall
 
 def detect_outfall(xbee,lock,event):
-	while not event.isSet():
-		while check_flowsensor() and check_levelsensor():
-			lock.aquire()
-	  		xbee.send_message('out\n')
-	  		sleep(0.5)
-	  		if xbee.receive_message() == 'oyes':
-	  			lock.release()
-	  			break
-	  		lock.release()
-	  	
+    while not event.isSet():
+        while check_flowsensor() and check_levelsensor():
+            print("outfall is occuring")
+            lock.aquire()
+            xbee.send_message('out\n')
+            sleep(0.5)
+            if xbee.receive_message() == 'oyes':
+                print("received outfall confirmation")
+                lock.release()
+                break
+            lock.release()
+        
 def detect_rainfall(xbee,lock,event):
-	while not event.isSet():
-		if get_tick():
-			lock.acquire()
-			create_trigger(xbee)
-			lock.release()
-			send_data(xbee,lock)
-		sleep(0.25)
+    while not event.isSet():
+        if get_tick():
+            print("rainguage invoked")
+            lock.acquire()
+            create_trigger(xbee)
+            lock.release()
+            send_data(xbee,lock)
+            print("sent the pool and rain data")
+        sleep(0.25)
 
 def create_trigger(xbee):
-	message = ""
-	while not message == "tyes":
-		xbee.send_message('tri\n')
-		message = xbee.receive_message()
+    message = ""
+    while not message == "tyes":
+        xbee.send_message('tri\n')
+        message = xbee.receive_message()
 
 def send_data(xbee,lock):
-	rain_val = rain_gauge.get_total_rainfall()
-	pool_val = 8
-	pool_val = 'p' + str(pool_val) + '\n'
-	rain_val = 'r' + str(rain_val) + '\n'
-	message = ""
-	lock.acquire()
-	create_trigger(xbee)
-	while not message == "ryes":
-		xbee.send_message(rain_val)
-		sleep(0.5)
-		xbee.send_message(pool_val)
-		sleep(0.5)
-		message  = xbee.receive_message()
-	lock.release()
+    rain_val = rain_gauge.get_total_rainfall()
+    pool_val = 8
+    pool_val = 'p' + str(pool_val) + '\n'
+    rain_val = 'r' + str(rain_val) + '\n'
+    message = ""
+    lock.acquire()
+    create_trigger(xbee)
+    while not message == "ryes":
+        xbee.send_message(rain_val)
+        sleep(0.5)
+        xbee.send_message(pool_val)
+        sleep(0.5)
+        message  = xbee.receive_message()
+    lock.release()
 
 def main():
-	lock = Lock()
-	event = Event()
-	port = xbee_usb_port()
-	if port != None:
-		try:
-			charlie_xbee = Transceiver(9600,port)
-			thread1 = Thread(target = detect_outfall, args = (charlie_xbee,lock,event, ))
-			thread2 = Thread(target = detect_rainfall, args = (charlie_xbee,lock,event,))
-			thread1.start()
-			thread2.start()
-		except KeyboardInterrupt:
-			event.set()
-			print("Ending the program")
-			thread1.join()
-			thread2.join()
+    lock = Lock()
+    event = Event()
+    port = xbee_usb_port()
+    if port != None:
+        try:
+            charlie_xbee = Transceiver(9600,port)
+            thread1 = Thread(target = detect_outfall, args = (charlie_xbee,lock,event, ))
+            thread2 = Thread(target = detect_rainfall, args = (charlie_xbee,lock,event,))
+            thread1.start()
+            thread2.start()
+        except KeyboardInterrupt:
+            event.set()
+            print("Ending the program")
+            thread1.join()
+            thread2.join()
+    else:
+        print('Missing xbee device')
 
-		else:
-			print("Missing xbee device")
+if __name__ == "__main__":
+    main()
