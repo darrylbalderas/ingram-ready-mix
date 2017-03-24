53b3151cb5774a0fc958d02c045029a68ca90111
db118 <d_b118@txstate.edu>
2017-03-23 21:01:01 -0500

    change timeout seconds

--- test_files/transceiver.py
+++ test_files/transceiver.py
@@ -14,11 +14,11 @@ class Transceiver:
     self.port_path = port_path
     self.baud_rate = baud_rate
     self.ser = serial.Serial(self.port_path, 
-                            self.baud_rate, timeout=3.0, 
+                            self.baud_rate, timeout=1.0, 
                             parity=serial.PARITY_NONE,
                             stopbits=serial.STOPBITS_ONE,
                             bytesize=serial.EIGHTBITS,
-                            writeTimeout=3.0)
+                            writeTimeout=1.0)
 
   def close_serial(self):
     '''
