1eef591c662295ec4d5b8e0420a2e4c498f734a6
db118 <d_b118@txstate.edu>
2017-04-18 20:35:23 -0500

    change timer look

--- test_files/test_lcd.py
+++ test_files/test_lcd.py
@@ -1,5 +1,6 @@
 from lcd import LCD
 import glob
+from time import sleep
 
 def lcd_serial_port():
   '''
@@ -19,7 +20,38 @@ def main():
 	port = lcd_serial_port()
 	if port != None:
 		lcd = LCD(port, 9600)
-		lcd.test_message()
+##		lcd.welcome_message()
+##		sleep(5)
+##		lcd.complete_message()
+##		sleep(5)
+##		lcd.missed_message()
+##		sleep(5)
+##		lcd.holding_restart(3)
+##		sleep(5)
+##		lcd.restart_message()
+##		sleep(5)
+		lcd.display_timer(0)
+		sleep(5)
+##		lcd.display_voltage(12.0,30,'complete',False)
+##		sleep(5)
+##		lcd.send_command('CLEAR')
+##		lcd.display_voltage(12.0,12,'missed',False)
+##		sleep(5)
+##		lcd.send_command('CLEAR')
+##		lcd.display_voltage(12.0,'None','None',False)
+##		sleep(5)
+##		lcd.send_command('CLEAR')		
+##		lcd.display_voltage(2.0,30,'complete',True)
+##		sleep(5)
+##		lcd.send_command('CLEAR')		
+##		lcd.display_voltage(2.0,12,'missed',True)
+##		sleep(5)
+##		lcd.send_command('CLEAR')
+##		lcd.display_voltage(2.0,'None','None',True)
+##		sleep(5)
+##		lcd.send_command('CLEAR')
+
+
 	else:
 		print("Missing LCD screen")
 
--- test_files/test_matrix.py
+++ test_files/test_matrix.py
@@ -2,7 +2,7 @@ from ledmatrix import LedMatrix
 
 def main():
     led_matrix = LedMatrix()
-    led_matrix.change_color(led_matrix.get_redImage())
+    led_matrix.change_color(led_matrix.clear_matrix())
 
 
 if __name__ == "__main__":
